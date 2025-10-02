1d62644c964f06b26f0e01f74e208b3e5199aaa5
Christie Warwick <bobcatfish@gmail.com>
2025-10-02 18:13:42 +0000

    fix: Update logs to be backward compatible even when weird
    The refactor for #10347 made the event structure explicit, while
    previously `...event` was sometimes used to determine the log
    attributes, which revealed several inconsistencies which this commit
    maintains for backward compatibility, though they were probably
    accidental:
    
    - eventName as well as event_name for ToolOutputTruncatedEvent
    - status_code as well as http.status_code for ApiErrorEvent
    - error, error_type in addition to error.message and error.type for ToolCallEvent

--- packages/core/src/telemetry/types.ts
+++ packages/core/src/telemetry/types.ts
@@ -295,8 +295,10 @@ export class ToolCallEvent implements BaseTelemetryEvent {
     };
 
     if (this.error) {
+      attributes['error'] = this.error;
       attributes['error.message'] = this.error;
       if (this.error_type) {
+        attributes['error_type'] = this.error_type;
         attributes['error.type'] = this.error_type;
       }
     }
@@ -458,6 +460,7 @@ export class ApiResponseEvent implements BaseTelemetryEvent {
       total_token_count: this.total_token_count,
       prompt_id: this.prompt_id,
       auth_type: this.auth_type,
+      status_code: this.status_code,
     };
     if (this.response_text) {
       attributes['response_text'] = this.response_text;
@@ -1107,6 +1110,7 @@ export class ToolOutputTruncatedEvent implements BaseTelemetryEvent {
     return {
       ...getCommonAttributes(config),
       'event.name': EVENT_TOOL_OUTPUT_TRUNCATED,
+      eventName: this.eventName,
       'event.timestamp': this['event.timestamp'],
       tool_name: this.tool_name,
       original_content_length: this.original_content_length,
